6172ee582fec89fd4b78ad56aa2648f1cca7da23
jiang.xiangkun <jiang.xiangkun@trs.com.cn>
2019-05-08 23:42:06 +0800

    setTimeout 传参写法

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # code-writing
+
 record some code
+用于记录一些代码的骚写法
---
+++ javascript/setTimeout.js
@@ -0,0 +1,17 @@
+/**
+ * 现阶段的知识层面
+ */
+
+// setTimeout 传参
+var fn = function(a, b, c) {
+    console.log(a, b, c);
+}
+var params = ['a', 'b', 'c'];
+
+// 三种写法：
+setTimeout.apply(this, [fn, 1000].concat(params));
+
+setTimeout(fn, 1000, ...params);
+
+setTimeout(fn, 1000, 'a', 'b', 'c')
+
